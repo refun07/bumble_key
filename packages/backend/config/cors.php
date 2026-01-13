7ee7d45e2ce1b0b958e63934a88a3aee252cd7c1
Ahmed-Hasib <hasibahmed999@gmail.com>
2026-01-13 16:20:45 +0600

    cors and others things updated

--- packages/backend/config/cors.php
+++ packages/backend/config/cors.php
@@ -1,25 +1,51 @@
 <?php
 
+// return [
+
+//     /*
+//     |--------------------------------------------------------------------------
+//     | Cross-Origin Resource Sharing (CORS) Configuration
+//     |--------------------------------------------------------------------------
+//     |
+//     | Here you may configure your settings for cross-origin resource sharing
+//     | or "CORS". This determines what cross-origin operations may execute
+//     | in web browsers. You are free to adjust these settings as needed.
+//     |
+//     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+//     |
+//     */
+
+//     'paths' => ['api/*'],
+
+//     'allowed_methods' => ['*'],
+
+//     'allowed_origins' => ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:8000'],
+
+//     'allowed_origins_patterns' => [],
+
+//     'allowed_headers' => ['*'],
+
+//     'exposed_headers' => [],
+
+//     'max_age' => 0,
+
+//     'supports_credentials' => true,
+
+// ];
+
+
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:8000'],
+    'allowed_origins' => [
+        'https://bumblekey.hasib-dev.com',   // Frontend (production)
+        'http://localhost:5173',             // Vite
+        'http://localhost:3000',             // React
+        'http://localhost:8000',             // Laravel local
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -30,5 +56,4 @@ return [
     'max_age' => 0,
 
     'supports_credentials' => true,
-
 ];
