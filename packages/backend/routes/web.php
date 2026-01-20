25dff41b99c053f378ef57f0957b8ba8eb570037
Ahmed-Hasib <hasibahmed999@gmail.com>
2026-01-20 18:19:41 +0600

    storage link added

--- packages/backend/routes/web.php
+++ packages/backend/routes/web.php
@@ -2,6 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
+
+Route::get('/link-storage', function () {
+    $target = storage_path('app/public');
+    $link = public_path('storage');
+
+    if (File::exists($link)) {
+        return Response::json([
+            'message' => 'Storage link already exists',
+        ]);
+    }
+
+    File::link($target, $link);
+
+    return Response::json([
+        'message' => 'Storage linked successfully',
+    ]);
+});
+
+
 Route::get('/', function () {
     return response()->json([
         'status' => 'BumbleKey API is running',
